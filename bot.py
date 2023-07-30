6a440def22f9efbb9f9d795da91236c2e232a898
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2023-07-30 19:54:41 +0300

    closures, carrying, decorators

--- bot.py
+++ bot.py
@@ -1,49 +1,37 @@
-PHONE_BOOK = {}
+import typing
+
+PHONE_BOOK: dict[str, str] = {}
 
 
-def input_error(func):
+def input_error(func: typing.Callable) -> typing.Callable:
 
-    def inner(list_of_request):
+    def inner(*args: str, **kwargs: dict) -> str:
 
-        if func.__name__ == "get_phone" and len(list_of_request) != 1:
-            print("DECORATOR Please just input 1 arg: correct name of user")
-            print("DECORATOR Trye again")
-            return
-
-        if func.__name__ != "get_phone":
-            if len(list_of_request) != 2:
-                print("DECORATOR Please enter 2 args: correct name and phone number")
-                print("DECORATOR Trye again")
-                return
-
-            elif len(list_of_request) == 2:
-                if not list_of_request[1].isdigit():
-                    print("DECORATOR Phone number must consist only of numbers")
-                    return
+        if len(args) == 2 and not args[1].isdigit():
+            return "DECORATOR Phone number must consist only of numbers"
 
         try:
-            result = func(list_of_request)
-            print("RESULT", result)
-            return result
+            result = func(*args, **kwargs)
+            return f"RESULT: {result}"
 
         except ValueError as error:
-            print(f"DECORATOR An error has occurred. Er: {error}")
+            return f"DECORATOR An error has occurred. Er: {error}"
         except IndexError as error:
-            print(f"DECORATOR Index not in range.Er: {error}")
+            return f"DECORATOR Index not in range.Er: {error}"
         except KeyError as error:
-            print(f"DECORATOR No such KEY.Er: {error}")
+            return f"DECORATOR No such KEY.Er: {error}"
+        except TypeError as error:
+            return f"DECORATOR Input correct all required arguments.Er: {error}"
 
     return inner
 
 
-def say_hello():
-    print("How can I help you?")
+def say_hello() -> str:
+    return "How can I help you?"
 
 
 @input_error
-def add_user(list_of_request):
-    name = list_of_request[0]
-    phone_num = list_of_request[1]
+def add_user(name: str, phone_num: str) -> str:
 
     if PHONE_BOOK.get(name):
         raise ValueError(
@@ -51,56 +39,51 @@ def add_user(list_of_request):
 
     PHONE_BOOK.update({name: phone_num})
 
-    print("add_user", PHONE_BOOK)
     return f"In phone book added user '{name}' with phone '{phone_num}'"
 
 
 @input_error
-def change_contact(list_of_request):
-    name = list_of_request[0]
-    phone_num = list_of_request[1]
+def change_contact(name: str, phone_num: str) -> str:
 
     if PHONE_BOOK.get(name):
         PHONE_BOOK[name] = phone_num
     else:
         raise ValueError(f"User '{name}' is not in phone book")
 
-    print("change_contact", PHONE_BOOK)
     return f"In phone book changed phone number of user '{name}' to '{phone_num}'"
 
 
 @input_error
-def get_phone(list_of_request):
-    name = list_of_request[0]
+def get_phone(name: str) -> str:
 
     if PHONE_BOOK.get(name):
         target_num = PHONE_BOOK[name]
     else:
         raise ValueError(f"User '{name}' is not in phone book.")
 
-    print("get_phone", target_num)
     return f"Target phone number for user '{name}'  is '{target_num}'"
 
 
-def show_all():
-    print("show_all", PHONE_BOOK)
+def show_all() -> str:
+    return f"show_all {PHONE_BOOK}"
 
 
-COMANDS = {
+COMANDS: dict[str, typing.Callable] = {
     'hello': say_hello,
     'add': add_user,
     'change': change_contact,
     'phone': get_phone,
-    'show_all': show_all,
+    'show all': show_all,
 }
 
 
-def get_handler(comand):
+def get_handler(comand: str) -> typing.Callable:
     return COMANDS[comand]
 
 
-def main():
+def main() -> None:
     launch = 0
+
     while True:
         if launch == 0:
             print("I'am a Bot")
@@ -110,26 +93,36 @@ def main():
         user_input = user_input.lower()
 
         if "show all" in user_input:
-            comand = "show_all"
+            comand = "show all"
             handler = get_handler(comand)
-            handler()
+            print(handler())
             continue
+
         elif "good bye" in user_input:
             print("Good bye!")
             break
 
-        user_input_list = user_input.split(" ")
-        comand = user_input_list[0]
+        list_of_request = user_input.strip().split(' ')
+        comand = list_of_request[0]
+        request_data = list_of_request[1:]
+
+        if request_data:
+            if len(request_data) >= 2 and request_data[len(request_data)-1].isdigit():
+                name = (" ").join(request_data[:len(request_data) - 1])
+                phone = request_data[-1]
+                request_data = [name, phone]
+
+            elif request_data[len(request_data)-1].isalpha():
+                name = (" ").join(request_data[:])
+                request_data = [name]
+
         if comand in ("close", "exit"):
             print("Good bye!")
             break
 
-        list_of_request = user_input_list[1:]
-
         try:
             handler = get_handler(comand)
-            handler = handler(list_of_request) if len(
-                list_of_request) > 0 else handler()
+            print(handler(*request_data))
         except KeyError as error:
             print(f"I don't know such a command.Er: {error}")
         continue
